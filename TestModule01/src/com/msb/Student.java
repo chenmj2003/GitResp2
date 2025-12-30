09ab20b91b7cfa0c13c2e1a66330a0f28c72eb6d
Chenmengjie <3196256143@qq.com>
2025-12-30 21:43:33 +0800

    我第二次进行数据提交。。。

--- TestModule01/src/com/msb/Student.java
+++ TestModule01/src/com/msb/Student.java
@@ -3,4 +3,8 @@ package com.msb;
 public class Student {
     private int age;
     private String name;
+    private double weight;
+    public void eat() {
+        System.out.println("我想吃饭。。。");
+    }
 }
